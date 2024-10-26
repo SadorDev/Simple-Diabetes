ff34e62a78efffd50ce685b4ba4cb7baaa3ac49e
Sador <Zerezghi@live.co.uk>
2024-10-26 22:56:04 +0100

    Added App Routes + Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-function App() {
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Logbook from "./pages/Logbook";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+
+const App = () => {
   return (
     <>
-      <h1> Hello World</h1>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="users" element={<Users />} />
+            <Route path="Settings" element={<Settings />} />
+            <Route path="logbook" element={<Logbook />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="#" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
