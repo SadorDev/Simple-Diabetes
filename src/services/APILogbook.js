444f2efc094495438c91acb6ce81765b810cc37d
Sador <Zerezghi@live.co.uk>
2024-10-31 07:57:31 +0000

    Logbook data added

--- src/services/APILogbook.js
+++ src/services/APILogbook.js
@@ -1,8 +1,9 @@
 import supabase from "./supabase";
 
 export async function getBloodGlucoseReadings() {
-  const { data, error } = await supabase.from("blood_glucose_log").select("*");
-
+  const { data, error } = await supabase
+    .from("blood_glucose_log")
+    .select("created_at, user_id, reading, insulin_units, timestamp, notes");
   if (error) {
     console.error("Data could not be loaded");
     throw new Error("BG readings could not be loaded");
