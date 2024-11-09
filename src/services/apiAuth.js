75f105a0449a882dd54178bcfe968b27d5d25e52
Sador <Zerezghi@live.co.uk>
2024-11-09 15:48:19 +0000

    Signup + user functionality from API

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,22 @@
 import supabase from "./supabase";
 
+// This signs up our user to supabase
+export async function signup({ email, password, fullName }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -11,6 +28,21 @@ export async function login({ email, password }) {
     throw new Error(error.message);
   }
 
-  console.log(data);
   return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
+
+export async function LoggedOut() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
