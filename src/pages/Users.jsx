e1302554acab414db41f4c2c244f9a21a56afadc
Sador <Zerezghi@live.co.uk>
2024-11-09 15:56:39 +0000

    Signup Form UI added

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,13 @@
+import Heading from "../ui/Heading";
+import SignUpForm from "../features//authentication/SignUpForm";
+
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <>
+      <Heading as="h1"> Create a New User</Heading>
+      <SignUpForm />
+    </>
+  );
 };
 
 export default Users;
