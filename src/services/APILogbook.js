80699bf6731fbc60f6e46774dff9bfaa57743edb
Sador <Zerezghi@live.co.uk>
2024-11-10 00:13:33 +0000

    Refactor getBloodGlucoseReadings to include userId parameter for fetching specific user's readings. Updated the function to require userId for filtering blood glucose log entries. Added error handling for cases when the user is not authenticated. Removed unnecessary retrieval of all readings for unauthenticated users.

--- src/services/APILogbook.js
+++ src/services/APILogbook.js
@@ -1,17 +1,15 @@
 import supabase from "./supabase";
 
-export async function getBloodGlucoseReadings() {
+export async function getBloodGlucoseReadings(userId) {
   const { data, error } = await supabase
     .from("blood_glucose_log")
-    .select("created_at, user_id, reading, insulin_units, timestamp, notes");
+    .select("created_at, user_id, reading, insulin_units, timestamp, notes")
+    .eq("user_id", userId);
+
   if (error) {
     console.error("Data could not be loaded");
     throw new Error("BG readings could not be loaded");
   }
 
-  const { user } = await supabase.auth.getUser();
-  if (!user) {
-    throw new Error("User is not authenticated");
-  }
   return data;
 }
