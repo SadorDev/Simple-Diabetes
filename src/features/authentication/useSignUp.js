73e48ce23ddf977bcf5caac3e216df8685c20793
Sador <Zerezghi@live.co.uk>
2024-12-27 02:53:34 +0000

    Added authentication state for signup feature - Solved issue

--- src/features/authentication/useSignUp.js
+++ src/features/authentication/useSignUp.js
@@ -2,19 +2,24 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signup as signupAPI } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
+import { useUser } from "./useUser";
 
 export function useSignUp() {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+  const { isAuthenticated } = useUser();
 
   const signup = async (credentials) => {
+    // Check Authentication state before signing up
+    if (isAuthenticated) {
+      toast.error("You are already logged in");
+      return;
+    }
     setIsLoading(true);
     try {
       const user = await signupAPI(credentials);
       localStorage.setItem("user", JSON.stringify(user.user));
-      toast.success(
-        "Account successfully created!"
-      );
+      toast.success("Account successfully created!");
       navigate("/dashboard", { replace: true });
       console.log(user);
     } catch (error) {
