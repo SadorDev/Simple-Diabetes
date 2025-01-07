4e55270266eb9a765727477b61672faf91ea650a
Sador <Zerezghi@live.co.uk>
2025-01-07 09:42:49 +0000

    Removed local storage and added error check

--- src/features/authentication/useSignUp.js
+++ src/features/authentication/useSignUp.js
@@ -2,26 +2,19 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signup as signupAPI } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
-import { useUser } from "./useUser";
 
 export function useSignUp() {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-  const { isAuthenticated } = useUser();
 
   const signup = async (credentials) => {
-    // Check Authentication state before signing up
-    if (isAuthenticated) {
-      toast.error("You are already logged in");
-      return;
-    }
     setIsLoading(true);
     try {
-      const user = await signupAPI(credentials);
-      localStorage.setItem("user", JSON.stringify(user.user));
+      const { user, error } = await signupAPI(credentials);
+      if (error) throw error;
       toast.success("Account successfully created!");
-      navigate("/dashboard", { replace: true });
-      console.log(user);
+
+      navigate("/dashboard", { replace: true }); 
     } catch (error) {
       console.error("Error:", error);
       toast.error(
